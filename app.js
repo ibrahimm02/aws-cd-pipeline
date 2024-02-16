8cfd88593686d9de09566ab2fba748e065458900
ibrahimm02 <ibm34427@gmail.com>
2024-02-16 23:56:28 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("AWS Continuous Delivery Pipeline Sample"));
+app.get("/", (req, res) =>
+  res.send(
+    "An Example AWS CD Pipeline with Source, Review, Build and Deploy stages!"
+  )
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
